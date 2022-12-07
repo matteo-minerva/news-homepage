6806cd33aabea7d97304c0f50a2f1ced86b63d6c
mminerva <m.minerva@outlook.it>
2022-12-07 17:00:41 +0100

    chore: burger logic

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
-console.log("linked")
+toggleBurgerMenu();
 
-export {}
+export {};
+
+function toggleBurgerMenu() {
+  const hamburgerClosed = document.querySelector(".hamburgerClosed");
+  const hamburgerOpened = document.querySelector(".hamburgerOpened");
+  const navbar = document.querySelector(".navbar");
+  const body = document.querySelector("body");
+
+  if (!hamburgerClosed) return false;
+  if (!hamburgerOpened) return false;
+  if (!navbar) return false;
+  if (!body) return false;
+
+  hamburgerClosed.addEventListener("click", () => {
+    navbar.classList.add("navbarActive");
+    body.style.overflow = "hidden";
+  });
+
+  hamburgerOpened.addEventListener("click", () => {
+    navbar.classList.remove("navbarActive");
+    body.style.overflow = "auto";
+  });
+
+  console.log(hamburgerClosed);
+
+  return true;
+}
